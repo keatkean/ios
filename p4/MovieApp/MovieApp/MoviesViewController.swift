d61f9e71f76fc15b27ae8e228dd60af1bb84f514
lihaiyun <li_haiyun@nyp.edu.sg>
2022-03-02 19:04:44 +0800

    table view

---
+++ p4/MovieApp/MovieApp/MoviesViewController.swift
@@ -0,0 +1,52 @@
+//
+//  MoviesViewController.swift
+//  MovieApp
+//
+//  Created by HaiYun on 2/3/22.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // First we query the table view to see if there are
+        // any UITableViewCells that can be reused. iOS will
+        // create a new one if there aren't any.
+        //
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "MovieCell", for: indexPath)
+        
+        // Using the re-used cell, or the newly created
+        // cell, we update the text label's text property.
+        //
+        cell.textLabel?.text = "This is some data"
+        
+        return cell
+    }
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
