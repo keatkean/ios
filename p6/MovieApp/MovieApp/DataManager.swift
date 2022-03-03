f037892b614b7bb4ce81e4140da2ef09deec0b32
lihaiyun <li_haiyun@nyp.edu.sg>
2022-03-03 14:28:27 +0800

    use data manager

---
+++ p6/MovieApp/MovieApp/DataManager.swift
@@ -0,0 +1,46 @@
+//
+//  DataManager.swift
+//  MovieApp
+//
+//  Created by HaiYun on 3/3/22.
+//
+
+import UIKit
+
+class DataManager: NSObject {
+    static var movieList : [Movie] = []
+
+    static func addMovie(movie: Movie) {
+        movieList.append(movie)
+    }
+    
+    static func getMovieList() -> [Movie] {
+        return movieList
+    }
+    
+    static func setupTestData() {
+        movieList = []
+        
+        // Source: https://www.gv.com.sg/GVMovies
+        movieList.append(Movie(
+            id: "3040",
+            name: "The Batman",
+            desc: "In his second year of fighting crime, Batman uncovers corruption in Gotham City that connects to his own family while facing a serial killer known as the Riddler.",
+            rating: 4,
+            image: "https://www.gv.com.sg/media/imagesresize/img3040.jpg"))
+        
+        movieList.append(Movie(
+            id: "3015",
+            name: "Spencer",
+            desc: "The marriage of Princess Diana and Prince Charles has long since grown cold. Though rumors of affairs and a divorce abound, peace is ordained for the Christmas festivities at the Queen’s Sandringham Estate. There's eating and drinking, shooting and hunting. Diana knows the game. But this year, things will be profoundly different. SPENCER is an imagining of what might have happened during those few fateful days.",
+            rating: 5,
+            image: "https://www.gv.com.sg/media/imagesresize/img3015.jpg"))
+        
+        movieList.append(Movie(
+            id: "3073",
+            name: "The Godfather (50th Anniversary)",
+            desc: "Paramount Pictures celebrates the 50th Anniversary of Francis Ford Coppola’s cinematic masterpiece, made available on 4K Ultra HD for the first time ever. Coppola’s masterful film adaptation of Mario Puzo’s novel chronicles the rise and fall of the Corleone family and the film trilogy is rightfully viewed as one of the greatest films in cinematic history. The film has been meticulously restored under the direction of Coppola, over the course of three years.",
+            rating: 5,
+            image: "https://www.gv.com.sg/media/imagesresize/img3073.jpg"))
+    }
+}
