318d83dd21732e806c1304df544d529ce271614f
lihaiyun <li_haiyun@nyp.edu.sg>
2022-03-03 11:40:59 +0800

    prepare p6

---
+++ p6/MovieApp/MovieApp/Movie.swift
@@ -0,0 +1,25 @@
+//
+//  Movie.swift
+//  MovieApp
+//
+//  Created by HaiYun on 2/3/22.
+//
+
+import UIKit
+
+class Movie : NSObject {
+    var id: String
+    var name: String
+    var desc: String
+    var rating: Int
+    var image: String
+    
+    init(id: String, name: String, desc: String, rating: Int, image: String)
+    {
+        self.id = id
+        self.name = name
+        self.desc = desc
+        self.rating = rating
+        self.image = image
+    }
+}
