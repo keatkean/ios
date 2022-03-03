0ff743d468ce78ca1f450573484741b590bec6f4
lihaiyun <li_haiyun@nyp.edu.sg>
2022-03-03 14:03:47 +0800

    connect view controller

---
+++ p6/MovieApp/MovieApp/AddMovieViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddMovieViewController.swift
+//  MovieApp
+//
+//  Created by HaiYun on 3/3/22.
+//
+
+import UIKit
+
+class AddMovieViewController: UIViewController {
+    
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var ratingTextField: UITextField!
+    @IBOutlet weak var imageTextField: UITextField!
+    @IBOutlet weak var descTextView: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
