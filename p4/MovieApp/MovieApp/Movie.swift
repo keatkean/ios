227cad7f8e6c13620ba336c41cf979e42dcb5e8e
lihaiyun <li_haiyun@nyp.edu.sg>
2022-03-03 11:39:48 +0800

    add movie id

--- p4/MovieApp/MovieApp/Movie.swift
+++ p4/MovieApp/MovieApp/Movie.swift
@@ -8,13 +8,15 @@
 import UIKit
 
 class Movie : NSObject {
+    var id: String
     var name: String
     var desc: String
     var rating: Int
     var image: String
     
-    init(name: String, desc: String, rating: Int, image: String)
+    init(id: String, name: String, desc: String, rating: Int, image: String)
     {
+        self.id = id
         self.name = name
         self.desc = desc
         self.rating = rating
