667e4a21c84ad409c8926e301d4040a8e7168171
lihaiyun <li_haiyun@nyp.edu.sg>
2023-02-09 14:56:52 +0800

    Update MoviesViewController.swift

--- MovieApp_P4b/MovieApp/MoviesViewController.swift
+++ MovieApp_P4b/MovieApp/MoviesViewController.swift
@@ -52,18 +52,18 @@ class MoviesViewController: UIViewController, UITableViewDataSource {
             image: "https://www.gv.com.sg/media/imagesresize/img3040.jpg"))
         
         movieList.append(Movie(
-            id: "3015",
-            name: "Spencer",
-            desc: "The marriage of Princess Diana and Prince Charles has long since grown cold. Though rumors of affairs and a divorce abound, peace is ordained for the Christmas festivities at the Queen’s Sandringham Estate. There's eating and drinking, shooting and hunting. Diana knows the game. But this year, things will be profoundly different. SPENCER is an imagining of what might have happened during those few fateful days.",
-            rating: 5,
-            image: "https://www.gv.com.sg/media/imagesresize/img3015.jpg"))
-        
-        movieList.append(Movie(
             id: "3073",
             name: "The Godfather (50th Anniversary)",
             desc: "Paramount Pictures celebrates the 50th Anniversary of Francis Ford Coppola’s cinematic masterpiece, made available on 4K Ultra HD for the first time ever. Coppola’s masterful film adaptation of Mario Puzo’s novel chronicles the rise and fall of the Corleone family and the film trilogy is rightfully viewed as one of the greatest films in cinematic history. The film has been meticulously restored under the direction of Coppola, over the course of three years.",
             rating: 5,
             image: "https://www.gv.com.sg/media/imagesresize/img3073.jpg"))
+        
+        movieList.append(Movie(
+            id: "4377",
+            name: "Titanic (25th Anniversary Re-Release)",
+            desc: "James Cameron's \"Titanic\" is an epic, action-packed romance set against the ill-fated maiden voyage of the R.M.S. Titanic; the pride and joy of the White Star Line and, at the time, the largest moving object ever built. She was the most luxurious liner of her era -- the \"ship of dreams\" -- which ultimately carried over 1,500 people to their death in the ice cold waters of the North Atlantic in the early hours of April 15, 1912.",
+            rating: 5,
+            image: "https://www.gv.com.sg/media/imagesresize/img4377.jpg"))
         
         // Do any additional setup after loading the view.
         tableView.dataSource = self
